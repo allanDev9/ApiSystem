7ccac600f7d01d68d6a9860f40ef20f891d56c09
allanDev9 <allanpenaguerra2003@gmail.com>
2025-04-29 18:06:38 -0600

    Renombrar propiedad Id a id en UserDto
    Se ha cambiado el nombre de la propiedad `Id` a `id` en la clase `UserDto`. Este cambio implica que la propiedad ahora utiliza una convención de nomenclatura en minúsculas en lugar de la convención PascalCase que se usaba anteriormente.

--- ApiSystem/Application/DTOs/UserDto.cs
+++ ApiSystem/Application/DTOs/UserDto.cs
@@ -2,7 +2,7 @@
 {
     public class UserDto
     {
-        public int Id { get; set; }
+        public int id { get; set; }
         public string name { get; set; }
         public string username { get; set; }
         public string password { get; set; }
